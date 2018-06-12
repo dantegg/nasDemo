612c19ccfb7d622bad2ceed9690a6c372ee32504
dantegg <runan.zhang@ele.me>
2018-06-12 14:33:53 +0800

    update

--- src/pages/app.vue
+++ src/pages/app.vue
@@ -4,10 +4,13 @@
             <el-col :span="12" :offset="6">
                 <div class="memory-head">
                     <h1>纪念日</h1>
-                    <p>记录重要的日子</p>
+                    <div>
+                        <p>记录重要的日子</p>
+                        <el-button type="primary" icon="el-icon-edit" circle></el-button>
+                    </div>
                 </div>
                 <div class="memory-body">
-                    <div v-for="item in data" :key="item">
+                    <div v-for="item in data" :key="item" class="memoryCard">
                         123
                     </div>
                 </div>

9df9ce512b99c10d4e648db01e893dbdc8abfd74
dantegg <luroufan327@hotmail.com>
2018-06-13 00:43:06 +0800

    update

--- src/pages/app.vue
+++ src/pages/app.vue
@@ -73,6 +73,21 @@ export default {
     },
     deleteItem(item) {
         console.log(item)
+        this.$confirm('此操作将删除这条记录, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.$message({
+            type: 'success',
+            message: '删除成功!'
+          });
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          })
+        })
     },
     query() {
         console.log('query')
